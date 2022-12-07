f214d7b82564e0b6661639ae0f7071861a2f8631
Beattris <beatriceebirim@gmail.com>
2022-12-07 16:48:26 +0100

    built out the first travel card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import { Travels } from './components/Travels';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Travels />
     </div>
   );
 }
